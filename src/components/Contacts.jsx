8f2da9ebc109a391c7c56a7c3762950d0736a5a5
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-02-07 02:36:11 +0200

    feat: rings decoration for contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Wrapper from "./Wrapper";
 import ContactsForm from "./ContactsForm";
+import Rings from "../assets/svg/header_rings.svg";
 
 const Contacts = () => {
     return (
         <div className="contacts__container">
             <Wrapper>
                 <section className="contacts">
-                        <div className="contacts__info">
-                            <h2>Contact</h2>
-                            <p className="secondary_text">I would love to hear about your project and how I could help.
-                                Please fill in the form, and I’ll get back to you as soon as possible.</p>
-                        </div>
-                        <ContactsForm/>
+                    <img src={Rings} alt="Rings" className="contscts__block__background"/>
+                    <div className="contacts__info">
+                        <h2>Contact</h2>
+                        <p className="secondary_text">I would love to hear about your project and how I could help.
+                            Please fill in the form, and I’ll get back to you as soon as possible.</p>
+                    </div>
+                    <ContactsForm/>
                 </section>
             </Wrapper>
         </div>
