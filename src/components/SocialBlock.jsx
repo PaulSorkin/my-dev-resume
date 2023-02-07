6e18c7eec5bd2dbcd087d198b0558576b170a218
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-02-07 03:04:57 +0200

    feat: add links to my social

--- src/components/SocialBlock.jsx
+++ src/components/SocialBlock.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { ReactComponent as Github } from '../assets/svg/github.svg'
-import { ReactComponent as Frontendmentor } from '../assets/svg/frontendmentor.svg'
 import { ReactComponent as Linkedin } from '../assets/svg/linkedin.svg'
-import { ReactComponent as Twitter } from '../assets/svg/twitter.svg'
+import { ReactComponent as Codewars } from '../assets/svg/codewars.svg'
 
 const SocialBlock = () => {
     return (
         <div className="social__block">
             <p className="logo">paulsorkin</p>
             <div className="social__block__links">
-                {/*<img src={Github} alt="Github" className="social_link"/>*/}
-                <Github className="social_link" />
-                <Frontendmentor className="social_link" />
-                <Linkedin className="social_link" />
-                <Twitter className="social_link" />
-                {/*<img src={Frontendmentor} alt="Frontendmentor" className="social_link"/>*/}
-                {/*<img src={Linkedin} alt="Linkedin" className="social_link"/>*/}
-                {/*<img src={Twitter} alt="Twitter" className="social_link"/>*/}
+                <a href="https://github.com/PaulSorkin"><Github className="social_link" /></a>
+                <a href="https://www.linkedin.com/in/paul-sorkin-tech/"><Linkedin className="social_link" /></a>
+                <a href="https://www.codewars.com/users/PaulSorkin"><Codewars className="social_link" /></a>
             </div>
         </div>
     );
