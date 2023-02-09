f33cecbc57ef7d48d864b25b135a5a8e4478ebc6
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-02-10 01:20:51 +0200

    fix: overflow hidden on wrapper instead of skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,14 +6,14 @@ import Wrapper from "./Wrapper";
 const Skills = () => {
     return (
         <Wrapper>
-            <section className={'skills'}>
+            <section className={"skills"}>
+                <img src={Rings} alt="Rings" className="skills__block__background"/>
                 <SkillBlock name="HTML" experience={4}/>
                 <SkillBlock name="CSS" experience={4}/>
                 <SkillBlock name="Javascript" experience={4}/>
                 <SkillBlock name="Accessibility" experience={4}/>
                 <SkillBlock name="React" experience={3}/>
                 <SkillBlock name="Sass" experience={3}/>
-                <img src={Rings} alt="Rings" className="skills__block__background"/>
             </section>
         </Wrapper>
     );
