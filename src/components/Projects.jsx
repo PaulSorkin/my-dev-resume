ea88564655e8ae1adc25693eaba30d6da7052979
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-02-10 16:10:22 +0200

    feat: add projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Link from "./Link";
 import Wrapper from "./Wrapper";
 import ProjectsItem from "./ProjectsItem";
-import ProjectPic1 from "../assets/img/projects/Bitmap.jpg"
+import World from "../assets/img/projects/World.png"
+import Blog from "../assets/img/projects/Blog-MERN.PNG"
+import Shelter from "../assets/img/projects/Shelter.PNG"
+import Firebase from "../assets/img/projects/Google Firebase Authentication.PNG"
+import Gallery from "../assets/img/projects/Image Gallery.PNG"
+import Sounds from "../assets/img/projects/Eco-sounds.PNG"
 import {HashLink} from "react-router-hash-link";
 
 const Projects = () => {
@@ -16,12 +21,12 @@ const Projects = () => {
                     </HashLink>
                 </div>
                 <div className="projects__block">
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
-                    <ProjectsItem ProjectPic={ProjectPic1} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'TODO WEB APP'} />
+                    <ProjectsItem ProjectPic={World} stack={['React', 'Styled Components', 'REST API']} name={'Where is the world?'} />
+                    <ProjectsItem ProjectPic={Blog} stack={['MongoDB', 'ExpressJS', 'React', 'Node.js', 'Redux', 'SASS', 'JWT authentication']} name={'Pauls\' Blog'} />
+                    <ProjectsItem ProjectPic={Shelter} stack={['HTML', 'SASS', 'JAVASCRIPT']} name={'Shelter'} />
+                    <ProjectsItem ProjectPic={Firebase} stack={['React', 'TypeScript', 'Redux Toolkit', 'Google Firebase Authentication']} name={'Google Firebase Authentication'} />
+                    <ProjectsItem ProjectPic={Gallery} stack={['HTML', 'CSS', 'JAVASCRIPT', 'REST API']} name={'Image Gallery'} />
+                    <ProjectsItem ProjectPic={Sounds} stack={['HTML', 'CSS', 'JAVASCRIPT']} name={'Eco-sounds'} />
                 </div>
             </section>
         </Wrapper>
